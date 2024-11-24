a95d39cce91d0bc0cca4fb8ea1c8a8e8d94425ea
Alika <dangolalika2017@gmail.com>
2024-11-24 08:52:24 +0545

    modified details page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -6,8 +6,8 @@ import { baseUrl } from "../../config/constant";
 
 const Details = () => {
   const { id } = useParams();
-  // const { data, API } = useApi();
-  const [detailData, setDetailData] = useState([]);
+
+  const [detailData, setDetailData] = useState({});
 
   const detailsAPI = async () => {
     try {
@@ -26,13 +26,24 @@ const Details = () => {
 
   return (
     <>
-      {/* {detailData.map((items)=>{
-    return(
-        <div key={items.id}>{items.title}</div>
-    )
-   })} */}
-
-      {detailData.title}
+      <div className="px-11 lg:px-44 ">
+        <div className="flex gap-5 mt-5 pt-3 border-black border-b-2 items-center">
+          <div className="w-[40px]">
+            <img className="w-full" src={detailData.thumbnailUrl} alt="" />
+          </div>
+          <div>
+            <h2 className="text-[14px]">AlbumId:{detailData.albumId}</h2>
+            <h2 className="text-[14px]">Product no. {detailData.id}</h2>
+          </div>
+        </div>
+        <div className="text-2xl lg:text-5xl font-bold mb-10 mt-5">
+          {detailData.title}
+        </div>
+        <div className="w-full mt-10 h-[600px] object-cover border ">
+          <img className="w-full h-full" src={detailData.url} alt="" />
+        </div>
+        <div className="my-5 text-xs"> {detailData.title}</div>
+      </div>
     </>
   );
 };
